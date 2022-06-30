430b1ce4d94f82c0602cfd6ecb7373cd9a30093a
jocelann02 <108388346+jocelann02@users.noreply.github.com>
2022-06-30 12:43:30 +0800

    Update Culalic.py

--- Culalic.py
+++ Culalic.py
@@ -3,7 +3,7 @@ while i == 0:
 
     
     print("===================")
-    print("Select operation.")
+    print("Select Operation.")
     print("1.Add (+)")
     print("2.Subtract (-)")
     print("3.Multiply (*)")
@@ -14,23 +14,23 @@ while i == 0:
     choice = input("Enter choice(1/2/3/4/5): ")
         
     if choice in ('1', '2', '3', '4', '5'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        N1 = float(input("Enter the first number: "))
+        N2 = float(input("Enter the second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", num1+num2)
+            print(N1, "+", N2, "=", N1+N2)
 
         elif choice == '2':
-            print(num1, "-", num2, "=", num1-num2)
+            print(N1, "-", N2, "=", N1-N2)
 
         elif choice == '3':
-            print(num1, "*", num2, "=", num1*num2)
+            print(N1, "*", N2, "=", N1*N2)
 
         elif choice == '4':
-            print(num1, "/", num2, "=", num1/num2)
+            print(N1, "/", N2, "=", N1/N2)
                 
         elif choice == '5':
-            print(num1, "%", num2, "=", int(num1%num2))
+            print(N1, "%", N2, "=", int(N1%N2))
     else:
         print("Invalid Input")
 
